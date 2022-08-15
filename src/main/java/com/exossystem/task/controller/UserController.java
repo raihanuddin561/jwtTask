3f32dcaf279b39f39129dc5e79aa67cb34338c48
raihanuddin561 <raihanuddin561@gmail.com>
2022-08-15 18:32:32 +0600

    update get and getAll method

--- src/main/java/com/exossystem/task/controller/UserController.java
+++ src/main/java/com/exossystem/task/controller/UserController.java
@@ -5,25 +5,33 @@ import com.exossystem.task.model.response.JwtResponse;
 import com.exossystem.task.service.UserService;
 import com.exossystem.task.utility.JwtUtility;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("users")
 public class UserController {
-    @GetMapping("/get")
-    public String hello(){
-        return "hello testing";
-    }
      @Autowired
     private JwtUtility jwtUtility;
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
     private UserService userService;
+    @GetMapping("/get")
+    public ResponseEntity<Integer> get(){
+        return new ResponseEntity<>(1, HttpStatus.OK);
+    }
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Integer>> getAll(){
+        return new ResponseEntity<>(List.of(1,2),HttpStatus.OK);
+    }
    @PostMapping("/authenticate")
     public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception {
         try {
